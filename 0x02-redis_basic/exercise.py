44708b56ed2c03eb547ed0cff6e7b8374c3ac082
Seamas007 <olufy007@gmail.com>
2023-07-19 13:42:21 +0100

    My commit

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key

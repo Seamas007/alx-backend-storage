f03b9ae78f85c2419cb783f605f3ea7e4e164593
Seamas007 <olufy007@gmail.com>
2023-07-19 15:08:55 +0100

    My commit

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,25 @@
+import requests
+import redis
+import time
+
+def get_page(url: str) -> str:
+    cache = redis.Redis()
+    count_key = f"count:{url}"
+    page_key = f"page:{url}"
+
+    # Check if the page is already cached
+    cached_page = cache.get(page_key)
+    if cached_page is not None:
+        return cached_page.decode()
+
+    # Increment the count of URL accesses
+    cache.incr(count_key)
+
+    # Fetch the page content
+    response = requests.get(url)
+    page_content = response.text
+
+    # Cache the page content with an expiration time of 10 seconds
+    cache.setex(page_key, 10, page_content)
+
+    return page_content

76ed7b3dfad808fdf0be2a91c30d983482a15e0f
Seamas007 <olufy007@gmail.com>
2023-07-19 14:40:48 +0100

    My commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Union
 
 class Cache:
     def __init__(self):
@@ -13,3 +11,17 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=int)

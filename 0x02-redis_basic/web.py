861a92ec6a55a65eb51b26cef99cb08e3e3717ff
Adefemi Oreyomi <111288660+Seamas007@users.noreply.github.com>
2023-07-21 12:32:51 +0100

    My commit

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -24,9 +24,9 @@ def data_cacher(method: Callable) -> Callable:
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
+        redis_store.set(f'count:{url}', 1)
         redis_store.setex(f'result:{url}', 10, result)
-        return result
+        return "OK"
     return invoker
 
 
